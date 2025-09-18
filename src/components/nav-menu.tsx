7af0c39c1e9b91a3a06635f661ff864ae20bb81c
Khoseyy <kristofhoseyy@gmail.com>
2025-09-18 13:05:56 +0100

    Fix siteConfig imports to use @/lib/site

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { siteConfig } from "@/lib/config";
+import { siteConfig } from "@/lib/site";
 import { motion } from "@/lib/motion-shim";
 import React, { useRef, useState } from "react";
 
